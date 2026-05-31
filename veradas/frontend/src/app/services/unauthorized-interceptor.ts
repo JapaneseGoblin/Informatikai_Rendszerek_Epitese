ee56aa771878687007eee7e6ea20c1f1c4144a06
JapaneseGoblin <szovati.oliver123@gmail.com>
2026-06-01 01:55:38 +0200

    asd
    asd

--- veradas/frontend/src/app/services/unauthorized-interceptor.ts
+++ veradas/frontend/src/app/services/unauthorized-interceptor.ts
@@ -1,5 +1,19 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpInterceptorFn, HttpErrorResponse } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
+import { AuthService } from './auth';
 
 export const unauthorizedInterceptor: HttpInterceptorFn = (req, next) => {
-  return next(req);
+  const authService = inject(AuthService);
+
+  return next(req).pipe(
+    catchError((err) => {
+      if (err instanceof HttpErrorResponse && err.status === 401) {
+        authService.removeToken();
+        authService.logout();
+      }
+      return throwError(() => err);
+    })
+  );
 };
